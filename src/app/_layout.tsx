2ffca525d8553733638808c7eff548928c057f79
Tyler Brecht <127113598+tyleravochoc@users.noreply.github.com>
2026-09-08 12:05:50 +0200

    Add Add Meal screen and navigation links.
    Register the add-meals route in the stack navigator and link to it from the Meals screen.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,16 +1,20 @@
-import { colors } from '@/styles/global';
-import { Stack } from 'expo-router';
+import { colors } from "@/styles/global";
+import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
     <Stack
       screenOptions={{
         headerStyle: { backgroundColor: colors.header },
-        headerTintColor: '#fff',
+        headerTintColor: "#fff",
       }}
     >
-      <Stack.Screen name='index' options={{ headerShown: false, title: 'Home' }} />
-      <Stack.Screen options={{ title: 'Meals' }} name='meals' />
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false, title: "Home" }}
+      />
+      <Stack.Screen options={{ title: "Meals" }} name="meals" />
+      <Stack.Screen options={{ title: "Add Meals" }} name="add-meals" />
     </Stack>
   );
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,14 +1,14 @@
-import HomeHeader from '@/componets/HomeHeader';
-import { globalStyles } from '@/styles/global';
-import { Link } from 'expo-router';
-import { ScrollView, Text } from 'react-native';
+import HomeHeader from "@/componets/HomeHeader";
+import { globalStyles } from "@/styles/global";
+import { Link } from "expo-router";
+import { ScrollView, Text } from "react-native";
 
 export default function HomeScreen() {
   return (
     <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-     <HomeHeader />
-      <Link href='/meals' style={{ fontSize: 18, color: '#007bff' }}>
+      <HomeHeader />
+      <Link href="/meals" style={{ fontSize: 18, color: "#007bff" }}>
         Go to Meals
       </Link>
     </ScrollView>
