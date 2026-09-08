681da0ada68e140d4b8b833e6328ddf94ee7a354
Tyler Brecht <127113598+tyleravochoc@users.noreply.github.com>
2026-09-08 12:01:48 +0200

    Add stack navigation and Meals screen.
    Configure Expo Router with a styled header, add a Meals route, and link to it from the home screen.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,16 @@
-import HomeHeader from "@/componets/HomeHeader";
-import { globalStyles } from "@/styles/global";
-import { StyleSheet, Text, View } from "react-native";
+import HomeHeader from '@/componets/HomeHeader';
+import { globalStyles } from '@/styles/global';
+import { Link } from 'expo-router';
+import { ScrollView, Text } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View style={globalStyles.container}>
+    <ScrollView style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-      <HomeHeader />
-    </View>
+     <HomeHeader />
+      <Link href='/meals' style={{ fontSize: 18, color: '#007bff' }}>
+        Go to Meals
+      </Link>
+    </ScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  date: {
-    fontSize: 14,
-    color: "#a0a0b0",
-    marginTop: 4,
-    marginBottom: 30,
-  },
-});
